ea46431d23ae0df2b74fa926bdb9566c516c5256
Andre Green <acgreen1226@gmail.com>
2015-07-03 13:15:34 -0700

    Updated manager to handle multiple devices generically

--- SEBLEInterface/SEBLEInterface/SEBLEInterfaceManager.h
+++ SEBLEInterface/SEBLEInterface/SEBLEInterfaceManager.h
@@ -15,7 +15,10 @@
 
 @protocol SEBLEInterfaceManagerDelegate <NSObject>
 
-- (void)bleInterfaceManager:(SEBLEInterfaceMangager *)interfaceManager didUpdateVales:(NSArray *)values;
+- (void)bleInterfaceManager:(SEBLEInterfaceMangager *)interfaceManager didUpdateDeviceValues:(NSDictionary *)values;
+- (void)bleInterfaceManager:(SEBLEInterfaceMangager *)interfaceManger discoveredPeriphealWithInfo:(NSDictionary *)info;
+- (void)bleInterfaceManager:(SEBLEInterfaceMangager *)interfaceManager connectedPeripheralNamed:(NSString *)peripheralName;
+
 
 @end
 
@@ -24,6 +27,9 @@
 @property (nonatomic, weak) id<SEBLEInterfaceManagerDelegate>delegate;
 
 + (id)manager;
+- (void)addDeviceWithUDID:(NSString *)UDID;
+- (void)removeDeviceWithUDID:(NSString *)UDID;
+- (BOOL)writeToDeviveWithUDID:(NSString *)UDID data:(u_int8_t)data;
 
 // remove me...just for testing
 - (void)runTests;
